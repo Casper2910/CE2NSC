1aad97b6db6133e6f8f73ca817e575a520053193
Casper2910 <casperbramm@hotmail.com>
2026-03-23 11:31:10 +0100

    include old functions in new mandelbrot object

--- miniproject_2/functions.py
+++ miniproject_2/functions.py
@@ -1,20 +1,113 @@
 import numpy as np
+from numba import njit
 import dask
+import threading
 
-def generate_array_shape(xmin, xmax, ymin, ymax, width, height):
-    # 2. Create grid of complex numbers 𝑐 over this region
-    x_values = np.linspace(xmin, xmax, width)
-    y_values = np.linspace(ymin, ymax, height)
 
-    # empty array for unit storage
-    return np.zeros((height, width), dtype=int)
+class Mandelbrot:
 
-def mandelbroot_dask(array, max_iter):
-    height, width = array.shape
+    def __init__(self):
+        self.xmin = -2,
+        self.xmax = 1,
+        self.ymin = -1.5,
+        self.ymax = 1.5,
+        self.width = 1024,
+        self.height = 1024,
+        self.x_values = np.linspace(self.xmin, self.xmax, self.width)
+        self.y_values = np.linspace(self.ymin, self.ymax, self.height)
+        self.array = np.zeros((self.height, self.width), dtype=int)
+    
+    def mandlebrot_naive(self):
+        array = self.array
+        for i in range(self.height):
+            for j in range(self.width):
+                
+                # 3. For each point c in grid:
+                # note to self: 1j is imaginary unit in python
+                c = self.x_values[j] + 1j * self.y_values[i]
+                
+                # > Initialize 𝑧0 =0
+                z = 0
+                
+                # > For 𝑛=0 to 𝑚𝑎𝑥_𝑖𝑡𝑒𝑟:
+                for n in range(self.max_iter):
+                    
+                    # > Compute 𝑧𝑛+1 =𝑧𝑛2 +𝑐
+                    z = z**2 + c
+                    
+                    # > If 𝑧𝑛+1 >2: Point escapes! Store 𝑛, break to next point
+                    if abs(z) > 2:
+                        array[i, j] = n
+                        break
+                else:
+                    # > If loop completes: Point is in set, store 𝑚𝑎𝑥_𝑖𝑡𝑒𝑟
+                    array[i, j] = self.max_iter
+        return array
+    
+    def mandlebrot_vectorized(self):
+        
+        array = self.array
+        # 3. For each point c in grid (perform operation on each element in array):
+        # note to self: 1j is imaginary unit in python
+        c = self.x_values[None, :] + 1j * self.y_values[:, None]
+        
+        # > Initialize 𝑧0 = 0 (for all points) (complex256 to prevent overflow of z)
+        z = np.zeros_like(c)
+        
+        # mask to keep track on updated indexes
+        mask = np.ones(c.shape, dtype=bool)
+        
+        # > For 𝑛=0 to 𝑚𝑎𝑥_𝑖𝑡𝑒𝑟:
+        for n in range(self.max_iter):
+            
+            # > Compute 𝑧𝑛+1 =𝑧𝑛2 +𝑐
+            z[mask] = z[mask]**2 + c[mask]
+            
+            # > If 𝑧𝑛+1 >2: Point escapes! Store 𝑛 
+            escaped = np.abs(z) > 2
+            newly_escaped = escaped & mask
+            array[newly_escaped] = n
+            mask[newly_escaped] = False
+            
+        # > If loop completes: Point is in set, store 𝑚𝑎𝑥_𝑖𝑡𝑒𝑟
+        array[mask] = self.max_iter
+
+        return array
+    
+    @njit
+    def mandlebrot_naive(self):
+        array = self.array
+        for i in range(self.height):
+            for j in range(self.width):
+                
+                # 3. For each point c in grid:
+                # note to self: 1j is imaginary unit in python
+                c = self.x_values[j] + 1j * self.y_values[i]
+                
+                # > Initialize 𝑧0 =0
+                z = 0
+                
+                # > For 𝑛=0 to 𝑚𝑎𝑥_𝑖𝑡𝑒𝑟:
+                for n in range(self.max_iter):
+                    
+                    # > Compute 𝑧𝑛+1 =𝑧𝑛2 +𝑐
+                    z = z**2 + c
+                    
+                    # > If 𝑧𝑛+1 >2: Point escapes! Store 𝑛, break to next point
+                    if abs(z) > 2:
+                        array[i, j] = n
+                        break
+                else:
+                    # > If loop completes: Point is in set, store 𝑚𝑎𝑥_𝑖𝑡𝑒𝑟
+                    array[i, j] = self.max_iter
+        return array
+    
+    def mandelbroot_paralel(self):
+        d = 2
+
+    def mandelbroot_dask(self):
+        nothing = 2
 
     
 if __name__ == "__main__":
-    xmin, xmax = -2, 1
-    ymin, ymax = -1.5, 1.5
-    width, height = 1024, 1024
-    max_iter = 100
+    m = Mandelbrot()
